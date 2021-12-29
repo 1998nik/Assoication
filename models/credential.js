e0175d0a3cd575d02ea71f773dae62538fee9efe
MTX <mtx@MTXs-MacBook-Pro.local>
2021-12-29 23:49:25 +0530

    association

---
+++ models/credential.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Credential extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Credential.init({
+    emailId:
+    {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    userid: 
+    {
+      type:DataTypes.INTEGER,
+      references:
+      {
+        model:
+        {
+          tableName:"UserDetails"
+        },
+        key:"id"
+      },
+      allowNull:false
+    }
+  }, {
+    sequelize,
+    modelName: 'Credential',
+  });
+  Credential.associate=function(models)
+  {
+    Credential.belongsTo(models.UserDetails,{
+      foreignKey:"userid"
+    })
+  };
+  return Credential;
+};
